083a7995f0990f525d9378a0faa2c20eefa0c42b
jimmy <jimmy.chen@cn.lorentnetworks.com>
2012-10-26 09:31:34 +0800

    videoclip

--- lcm/src/com/lorent/model/VideoClipBean.java
+++ lcm/src/com/lorent/model/VideoClipBean.java
@@ -6,16 +6,37 @@ public class VideoClipBean extends BaseModel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String videoClipUrl;//视频存放url地址
+	private String videoClipUrlHigh;//高清视频存放url地址
+	private String videoClipUrlStandard;//标清视频存放url地址
 	private String thumbnailUrl;//视频缩略图url地址
 	private String title;//标题
 	private String description;//描述
 	private String category;//类别
-	public String getVideoClipUrl() {
-		return videoClipUrl;
+	private String createrName;
+	public String getCreaterName() {
+		return createrName;
 	}
-	public void setVideoClipUrl(String videoClipUrl) {
-		this.videoClipUrl = videoClipUrl;
+	public void setCreaterName(String createrName) {
+		this.createrName = createrName;
+	}
+	public String getCreaterNo() {
+		return createrNo;
+	}
+	public void setCreaterNo(String createrNo) {
+		this.createrNo = createrNo;
+	}
+	private String createrNo;
+	public String getVideoClipUrlHigh() {
+		return videoClipUrlHigh;
+	}
+	public void setVideoClipUrlHigh(String videoClipUrlHigh) {
+		this.videoClipUrlHigh = videoClipUrlHigh;
+	}
+	public String getVideoClipUrlStandard() {
+		return videoClipUrlStandard;
+	}
+	public void setVideoClipUrlStandard(String videoClipUrlStandard) {
+		this.videoClipUrlStandard = videoClipUrlStandard;
 	}
 	public String getThumbnailUrl() {
 		return thumbnailUrl;
