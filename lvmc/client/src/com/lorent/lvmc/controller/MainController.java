5c8b5a1a3156d8e9ff1f63c49ed08a06c8b61128
jimmy <jimmy.chen@cn.lorentnetworks.com>
2013-01-23 09:19:08 +0800

    register user

--- lvmc/client/src/com/lorent/lvmc/controller/MainController.java
+++ lvmc/client/src/com/lorent/lvmc/controller/MainController.java
@@ -190,8 +190,7 @@ public class MainController extends BaseController{
     
     public void showRegisterUserDialog() throws Exception{
     	LoginFrame loginFrame = ViewManager.getComponent(LoginFrame.class);
-    	RegisterUserDialog dialog = ViewManager.getComponent(RegisterUserDialog.class,new Class[] { java.awt.Frame.class,
-			boolean.class }, new Object[] { loginFrame, false });
+    	RegisterUserDialog dialog = new RegisterUserDialog(loginFrame, true);
     	ViewManager.setWindowCenterLocation(dialog);
     	dialog.setVisible(true);
     }
@@ -200,34 +199,34 @@ public class MainController extends BaseController{
     	//检测输入
     	String username = dialog.getUsernameInput().getText().trim();
     	if (username == null || username.equals("")) {
-			JOptionPane.showMessageDialog(null, "昵称不能为空");
+			JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.needusername"));
 			return false;
 		}
     	else{
     		if (username.length() > 20) {
-				JOptionPane.showMessageDialog(null, "昵称长度超出限制");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.morethan20"));
 				return false;
 			}
     	}
     	String realname = dialog.getRealnameInput().getText().trim();
     	if (realname == null || realname.equals("")) {
-			JOptionPane.showMessageDialog(null, "真实姓名不能为空");
+			JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.needrealname"));
 			return false;
 		}
     	else{
     		if (realname.length() > 30) {
-				JOptionPane.showMessageDialog(null, "真实姓名长度超出限制");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.realname.morethan30"));
 				return false;
 			}
     	}
     	String email = dialog.getEmailInput().getText().trim();
     	if (email ==  null || email.equals("")) {
-			JOptionPane.showMessageDialog(null, "邮箱地址不能为空");
+			JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.needemail"));
 			return false;
 		}
     	else{
     		if (email.length() > 50) {
-				JOptionPane.showMessageDialog(null, "邮箱地址长度超出限制");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.email.morethan50"));
 				return false;
 			}
     		else{
@@ -235,27 +234,32 @@ public class MainController extends BaseController{
     			 pattern = Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
     			 Matcher matcher = pattern.matcher(email);
     			 if (!matcher.matches()) {
-					JOptionPane.showMessageDialog(null, "邮箱地址格式不正确");
+					JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.email.formaterror"));
 					return false;
 				}
     		}
     	}
     	String serverip = dialog.getServerIpInput().getText().trim();
     	if (serverip == null || serverip.equals("")) {
-    		JOptionPane.showMessageDialog(null, "服务器地址不能为空");
+    		JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.needserverip"));
     		return false;
 			
 		}
     	else{
     		if (serverip.length() > 15) {
-				JOptionPane.showMessageDialog(null, "服务器地址长度超出限制");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.serverip.morethan15"));
 				return false;
 			}
     		else{
-    			Pattern pattern = Pattern.compile("\\b((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\b");
+    			//"\\b((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\b"
+    			String regex = "^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|[1-9])\\."
+                    + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+                    + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)\\."
+                    + "(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|\\d)$";
+    			Pattern pattern = Pattern.compile(regex);
     			Matcher matcher = pattern.matcher(serverip);
     			if (!matcher.matches()) {
-					JOptionPane.showMessageDialog(null, "服务器地址格式不正确");
+					JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.serverip.formaterror"));
 					return false;
 				}
     		}
@@ -263,20 +267,20 @@ public class MainController extends BaseController{
     	String password = dialog.getPasswdInput().getText();
     	String repassword = dialog.getRePasswdInput().getText();
     	if (password == null || password.equals("")) {
-			JOptionPane.showMessageDialog(null, "请输入密码");
+			JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.needpsw"));
 			return false;
 		}
     	else{
     		if (password.length() < 6) {
-				JOptionPane.showMessageDialog(null, "密码长度不能少于6位");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.psw.lessthan6"));
 				return false;
 			}
     		else if(password.length() > 15){
-    			JOptionPane.showMessageDialog(null, "密码长度超出限制");
+    			JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.psw.morethan15"));
     			return false;
     		}
     		if (!password.equals(repassword)) {
-				JOptionPane.showMessageDialog(null, "两次输入的密码不相同");
+				JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.psw.different"));
 				return false;
 			}
     	}
@@ -285,11 +289,43 @@ public class MainController extends BaseController{
     
     public void doRegisterUser(RegisterUserDialog dialog) throws Exception{
     	if (checkUserInput(dialog)) {
-			
-    		
-    		
-    		
-    		
+    		String username = dialog.getUsernameInput().getText().trim();
+    		String password = dialog.getPasswdInput().getText();
+    		String realname = dialog.getRealnameInput().getText().trim();
+    		String email = dialog.getEmailInput().getText().trim();
+    		String phone = dialog.getPhoneInput().getText().trim();
+    		String gender = dialog.getGenderComboBox().getSelectedItem().toString();
+    		if (gender.equals("男")) {
+				gender = "male";
+			}
+    		else{
+    			gender = "female";
+    		}
+    		String mobile = dialog.getMobileInput().getText().trim();
+    		String department = "";
+    		String position = "";
+    		String code = "";
+    		String lcc_account = "";
+			String serverIP = dialog.getServerIpInput().getText().trim();
+			try {
+				boolean registerUser = Launcher.getLCMUtil(serverIP).registerUser(username, password, realname, email, phone, gender, mobile, department, position, code, lcc_account, serverIP);
+				if (registerUser) {
+					JOptionPane.showMessageDialog(null, StringUtil.getUIString("registerUser.reg.sendmailok"));
+					dialog.dispose();
+				}
+				else{
+					JOptionPane.showMessageDialog(null, StringUtil.getErrorString("registerUser.reg.error"));
+				}
+			} catch (Exception e) {
+				String message = e.getMessage();
+				String substring = message.substring(message.indexOf(":")+1);
+				if (substring.trim().equals("Connection refused: connect")) {
+					substring = StringUtil.getErrorString("registerUser.reg.connectRefused");
+				}
+				JOptionPane.showMessageDialog(null, substring);
+				log.error("注册失败",e);
+				e.printStackTrace();
+			}
 		}
     }
     
